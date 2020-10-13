e9001932e964dc724045915fb499ab3c61998acd
lisambamberg <lisambamberg@gmail.com>
2020-10-12 20:48:26 -0500

    Added Info section to HTML, color to CSS, outline for JS

---
+++ app.js
@@ -0,0 +1,29 @@
+class Shape {
+    constructor() {
+
+    }
+}
+
+class Circle extends Shape {
+    constructor() {
+
+    }
+}
+
+class Triangle extends Shape {
+    constructor() {
+
+    }
+}
+
+class Rectangle extends Shape {
+    constructor() {
+
+    }
+}
+
+class Square extends Shape {
+    constructor() {
+
+    }
+}
