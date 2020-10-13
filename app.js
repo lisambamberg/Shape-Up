7ace6ab812f3b66859d24a13d6c8aa2be7a8e72e
lisambamberg <lisambamberg@gmail.com>
2020-10-12 22:35:14 -0500

    Added button ids, reformated JS

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+const shapeContainer = document.getElementById("shapeContainer");
+const rectBtn = document.getElementById("rectBtn");
+const squareBtn = document.getElementById("squareBtn");
+const circleBtn = document.getElementById("circleBtn");
+const triangleBtn = document.getElementById("triangleBtn");
+
 class Shape {
     constructor() {
 
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(width, height) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
+}
+
+class Square extends Shape {
+    constructor(sideLength) {
+        super()
     }
 }
 
@@ -16,14 +36,3 @@ class Triangle extends Shape {
     }
 }
 
-class Rectangle extends Shape {
-    constructor(width, height) {
-        super()
-    }
-}
-
-class Square extends Shape {
-    constructor(sideLength) {
-        super()
-    }
-}
