0c4cecd04be938b01d6b156d507bd169d90fbe8e
lisambamberg <lisambamberg@gmail.com>
2020-10-12 23:37:50 -0500

    Edited CSS, Added rectangle and square basic outlines

--- app.js
+++ app.js
@@ -5,34 +5,50 @@ const circleBtn = document.getElementById("circleBtn");
 const triangleBtn = document.getElementById("triangleBtn");
 
 class Shape {
-    constructor() {
-
+    constructor(x, y) {
+        this.shape = document.getElementById("div");
+        this.shape.style.left = `${x}px`,
+            this.shape.style.top = `${y}px`
+        shapeContainer.appendChild(this.shape);
     }
 }
 
 class Rectangle extends Shape {
-    constructor(width, height) {
-        super();
+    constructor(x, y, rectwidth, rectheight) {
+        super(x, y,);
         this.width = width;
         this.height = height;
+        this.shape.addClass("rectangle");
+        this.shape.style.width = `${rectwidth}px`,
+            this.shape.style.height = `${rectheight}px`
+        this.shape.style.display = "inherit";
+        shapeContainer.append(this.shape)
     }
 }
 
+
 class Square extends Shape {
-    constructor(sideLength) {
+    constructor(x, y, squareSideLength) {
         super()
+        this.shape.addClass("sqaure");
+        this.shape.style.width = `${squareSideLength}px`,
+            this.shape.style.height = `${squareSideLength}px`,
+            this.shape.style.display = "inherit";
+        shapeContainer.append(this.shape);
     }
 }
 
 class Circle extends Shape {
     constructor(radius) {
         super()
+        this.shape.addClass("circle");
     }
 }
 
 class Triangle extends Shape {
     constructor(height) {
         super()
+        this.shape.addClass("triangle");
     }
 }
 
