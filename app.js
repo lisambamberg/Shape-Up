1be00b0d455cad7fe163f4b385e8977d01d700b8
lisambamberg <lisambamberg@gmail.com>
2020-10-13 00:18:15 -0500

    Refactored HTML, Added circle and triangle beginning logic

--- app.js
+++ app.js
@@ -14,22 +14,19 @@ class Shape {
 }
 
 class Rectangle extends Shape {
-    constructor(x, y, rectwidth, rectheight) {
-        super(x, y,);
-        this.width = width;
-        this.height = height;
+    constructor(x, y, rectWidth, rectHeight) {
+        super(x, y);
         this.shape.addClass("rectangle");
-        this.shape.style.width = `${rectwidth}px`,
-            this.shape.style.height = `${rectheight}px`
+        this.shape.style.width = `${rectWidth}px`,
+            this.shape.style.height = `${rectHeight}px`
         this.shape.style.display = "inherit";
         shapeContainer.append(this.shape)
     }
 }
 
-
 class Square extends Shape {
     constructor(x, y, squareSideLength) {
-        super()
+        super(x, y)
         this.shape.addClass("sqaure");
         this.shape.style.width = `${squareSideLength}px`,
             this.shape.style.height = `${squareSideLength}px`,
@@ -39,16 +36,23 @@ class Square extends Shape {
 }
 
 class Circle extends Shape {
-    constructor(radius) {
-        super()
+    constructor(x, y, circleRadius) {
+        super(x, y)
         this.shape.addClass("circle");
+        this.shape.style.width = `${circleRadius * 2}px`,
+            this.shape.style.height = `${circleRadius * 2}px`,
+            this.shape.style.display = "inherit";
+        shapeContainer.append(this.shape);
     }
 }
 
 class Triangle extends Shape {
-    constructor(height) {
-        super()
+    constructor(x, y) {
+        super(x, y)
         this.shape.addClass("triangle");
+        this.shape.style.borderBottom = `${this.width}px`,
+            this.shape.style.borderRight = `${this.height}px`,
+            this.shape.style.display = "inherit";
+        shapeContainer.append(this.shape)
     }
 }
-
