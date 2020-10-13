c704210b8b90001961f0ea09f8cea0c24b9ca2ef
lisambamberg <lisambamberg@gmail.com>
2020-10-13 01:28:50 -0500

    Added EventListener for buttons to create new shapes

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const circleBtn = document.getElementById("circleBtn");
 const triangleBtn = document.getElementById("triangleBtn");
 
 class Shape {
-    constructor(x, y) {
+    constructor(width, height) {
+        this.Width = width;
+        this.Height = height;
         this.shape = document.getElementById("div");
         this.shape.style.left = `${x}px`,
             this.shape.style.top = `${y}px`
@@ -14,45 +16,46 @@ class Shape {
 }
 
 class Rectangle extends Shape {
-    constructor(x, y, rectWidth, rectHeight) {
-        super(x, y);
+    constructor(rectWidth, rectHeight) {
+        super(width, height);
         this.shape.addClass("rectangle");
         this.shape.style.width = `${rectWidth}px`,
             this.shape.style.height = `${rectHeight}px`
-        this.shape.style.display = "inherit";
         shapeContainer.append(this.shape)
     }
 }
 
 class Square extends Shape {
-    constructor(x, y, squareSideLength) {
-        super(x, y)
+    constructor(squareSideLength) {
+        super()
         this.shape.addClass("sqaure");
         this.shape.style.width = `${squareSideLength}px`,
             this.shape.style.height = `${squareSideLength}px`,
-            this.shape.style.display = "inherit";
-        shapeContainer.append(this.shape);
+            shapeContainer.append(this.shape);
     }
 }
 
 class Circle extends Shape {
-    constructor(x, y, circleRadius) {
-        super(x, y)
+    constructor(circleRadius) {
+        super()
         this.shape.addClass("circle");
         this.shape.style.width = `${circleRadius * 2}px`,
             this.shape.style.height = `${circleRadius * 2}px`,
-            this.shape.style.display = "inherit";
-        shapeContainer.append(this.shape);
+            shapeContainer.append(this.shape);
     }
 }
 
 class Triangle extends Shape {
-    constructor(x, y) {
-        super(x, y)
+    constructor(width, height) {
+        super(width, height)
         this.shape.addClass("triangle");
-        this.shape.style.borderBottom = `${this.width}px`,
-            this.shape.style.borderRight = `${this.height}px`,
-            this.shape.style.display = "inherit";
-        shapeContainer.append(this.shape)
+        this.shape.style.borderBottom = `${triangleSide}px`,
+            this.shape.style.borderRight = `${triangleSide}px`,
+            shapeContainer.append(this.shape)
     }
 }
+
+rectBtn.addEventListener("click", () => { new Rectangle(rectWidth.value, rectHeight.value) });
+squareBtn.addEventListener("click", () => { new Square(squareSideLength.value, squareSideLength.value) });
+circleBtn.addEventListener("click", () => { new Circle((circleRadius.value * 2), (circleRadius.value * 2)) });
+triangleBtn.addEventListener("click", () => { new Triangle(triangleSide.value, triangleSide.value) });
